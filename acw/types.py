72d7937961d9da5449bed550fe9f594997ab0a14
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2015-04-22 22:33:07 +0200

    Move value validation logic out of Type.__init__

--- acw/types.py
+++ acw/types.py
@@ -1,12 +1,15 @@
 class Type(object):
     def __init__(self, default=None):
         if default is not None:
-            if not isinstance(default, (int, long)):
+            if not self.validate(default):
                 raise ValueError
             self.default = default
 
 
 class IntegerType(Type):
+    def validate(self, value):
+        return isinstance(value, (int, long))
+
     def loads(self, raw):
         return int(raw)
 
