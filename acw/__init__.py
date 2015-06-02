e3124bd44eb2b6ae74ebdda71c2c37c93d40f98b
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2015-06-03 00:02:39 +0200

    Add module docstring for acw.__init__

--- acw/__init__.py
+++ acw/__init__.py
@@ -1,3 +1,13 @@
+"""
+The Awesome Config Wrapper
+
+This is an end-all wrapper around the ConfigParser library, aiming to add
+at the minimum the following features:
+    - typechecking
+    - value defaulting
+    - validation
+"""
+
 from .config import Config
 from .section import ConfigSection
 from .types import Type, IntegerType
